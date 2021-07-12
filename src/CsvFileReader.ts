c661674590d103a98e5b98927dee66c118a9fe0d
Bogdan <maiskiychavk@gmail.com>
2021-07-12 22:31:53 +0300

    [refactoring to interface]

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,20 +1,17 @@
 import fs from 'fs';
 
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+export class CsvFileReader {
+  data: string[][] = [];
   
   constructor(public fileName: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.fileName, {
       encoding: 'utf-8'
     })
-      .split('\n')
-      .map((row: string): string[] => {
-        return row.split(',');
-      })
-      .map(this.mapRow);
+    .split('\n')
+    .map((row: string): string[] => {
+      return row.split(',');
+    })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('footbal.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (const match of reader.data) {
+for (const match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
