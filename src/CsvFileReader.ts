c5b4cec2a3d98dcd549856c9f42c1794e12be644
Bogdan <maiskiychavk@gmail.com>
2021-07-12 22:01:14 +0300

    [refactoring to abstract class with generic]

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,9 +1,11 @@
 import fs from 'fs';
 
-export class CsvFileReader {
-  data: string[][] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
   
   constructor(public fileName: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs.readFileSync(this.fileName, {
@@ -12,6 +14,7 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      });
+      })
+      .map(this.mapRow);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 
 let manUnitedWins = 0;
 
