0df3e0c372e052e660cd6828f1d2b6d53fc1dc39
Bogdan <maiskiychavk@gmail.com>
2021-07-14 22:32:19 +0300

    [add html report]

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { CsvFileReader } from './CsvFileReader';
 import { WinsAnalysis } from './analyzer/WinsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
@@ -11,3 +12,6 @@ matchReader.load();
 
 const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
 summary.buildAndPrintReport(matchReader.matches);
+
+const summary2 = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+summary2.buildAndPrintReport(matchReader.matches);
