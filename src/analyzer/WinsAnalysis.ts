d3cc01da2334fd152c4270419412ded335cce047
Bogdan <maiskiychavk@gmail.com>
2021-07-14 22:26:52 +0300

    [add print data composition]

---
+++ src/analyzer/WinsAnalysis.ts
@@ -0,0 +1,21 @@
+import { MatchData } from '../MatchData';
+import { Analyzer } from '../Summary';
+import { MatchResult } from '../MatchResult';
+
+export class WinsAnalysis implements Analyzer {
+  constructor(public team: string) {}
+
+  run(matches: MatchData[]): string {
+    let wins = 0;
+
+    for (const match of matches) {
+      if (match[1] === this.team && match[5] === MatchResult.HomeWin) {
+        wins++;
+      } else if (match[2] === this.team && match[5] === MatchResult.AwayWin) {
+        wins++;
+      }
+    }
+
+    return `Team ${this.team} won ${wins} times`;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
-import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
+import { WinsAnalysis } from './analyzer/WinsAnalysis';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('footbal.csv');
+const csvFileReader = new CsvFileReader('football.csv');
+
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-let manUnitedWins = 0;
-
-for (const match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} times`);
+const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
